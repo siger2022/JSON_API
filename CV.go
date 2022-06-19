e8e69e799eb8391d940642497286bb4bbfbc2f77
Regis Wong Hämäläinen <regis.wonghamalainen@aalto.fi>
2022-06-19 11:45:12 +0300

    CV

---
+++ CV.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type Address struct {
+	Country string `json:"Country:"`
+	City    string `json:"City:"`
+	Street  string `json:"Street:"`
+}
+
+type user struct {
+	First     string `json:"First name:"`
+	Last      string `json:"Last name:"`
+	Age       int    `json:"Age:"`
+	Language  string `json:"languages:"`
+	Address   Address
+	Education string     `json:"Education:"`
+	CreatedAt time.Time  `json:"Created at:"`
+	DeletedAt *time.Time `json:"deleted_at:"`
+}
+
+func main() {
+	t := time.Now()
+	users := []user{
+		{First: "Ilari", Last: "Skywalker", Age: 43, Language: "Japanese, English, Finnish and Cantonese", Address: Address{"Finland", "Vanta", "Koskenkorvakatu 90, 22"}, Education: "Energy techology", CreatedAt: time.Now(), DeletedAt: &t},
+		{First: "Sabrina", Last: "The builder", Age: 30, Language: "English, Arabic, and Latvian", Address: Address{"Latvia", "Liepaja", "Vilandes iela"}, Education: "Political science", CreatedAt: time.Now(), DeletedAt: &t},
+	}
+
+	output, err := json.MarshalIndent(users, ">>", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(output))
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-## JSON encoding and decoding
-
-Goal: To practice JSON encoding and decoding
-file: recipe_encoding.go 
